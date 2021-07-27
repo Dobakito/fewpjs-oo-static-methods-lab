9f88c48184991e7b0f06b42f3ed5dfa6946697d8
Dobakito <mdobak44@icloud.com>
2021-07-26 19:06:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+  static titleize(string){
+    let newString = Formatter.capitalize(string);
+    let stringArray = newString.split(' ');
+    stringArray = stringArray.map(function(str) {
+      if (str.match(/^(the|a|an|but|of|and|for|at|by|from)$/)) {
+        return str
+      } else {
+        return Formatter.capitalize(str);
+      }
+   });
+   return stringArray.join(' ');
+  }
 }
